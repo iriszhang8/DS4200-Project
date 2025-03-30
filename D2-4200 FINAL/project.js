a85df2a99018988d159b47c6ecf119da2429adff
Dmurphy21 <54049445+Dmurphy21@users.noreply.github.com>
2025-03-30 19:58:54 -0400

    Add files via upload

--- D2-4200 FINAL/project.js
+++ D2-4200 FINAL/project.js
@@ -1,7 +1,8 @@
 // Example interactive feature
 window.addEventListener("DOMContentLoaded", () => {
     const msg = document.createElement("p");
-    msg.textContent = "Thank you for exploring our Costco analysis!";
+    msg.innerHTML = "Thank you for exploring our Costco analysis!<br>Dhillon Murphy, Iris Zhang, Yvonne PengSyuan Lin (Group 7)";
+    msg.style.textAlign = "center";
     msg.style.fontWeight = "bold";
     msg.style.color = "#1e3d59";
     document.body.appendChild(msg);
