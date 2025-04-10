fc0f73223c2a1cc1f0fdcc79ee1346e436f33d20
Dmurphy21 <54049445+Dmurphy21@users.noreply.github.com>
2025-04-10 17:32:11 -0400

    Delete D2-4200 FINAL V2 directory

--- D2-4200 FINAL V2/project.js
+++
@@ -1,9 +0,0 @@
-// Example interactive feature
-window.addEventListener("DOMContentLoaded", () => {
-    const msg = document.createElement("p");
-    msg.innerHTML = "Thank you for exploring our Costco analysis!<br>Dhillon Murphy, Iris Zhang, Yvonne PengSyuan Lin (Group 7)";
-    msg.style.textAlign = "center";
-    msg.style.fontWeight = "bold";
-    msg.style.color = "#1e3d59";
-    document.body.appendChild(msg);
-});
